faca57b937390ccea40d5cc5c54dfe072f414eea
DilipSC <dilipchakravarthi570@gmail.com>
2024-11-26 00:19:07 +0530

    final done

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -63,7 +63,7 @@ export default function Main() {
                     <div className="text-black mt-6 rounded-lg bg-white p-6">
                         <h3 className="font-bold">Shortened URL:</h3>
                         <a href={`https://url-shortener-1-l8gp.onrender.com/${surl}`} target="_blank" rel="noopener noreferrer">
-                            {`https://url-shortener-1-l8gp.onrender.com${surl}`}
+                            {`https://url-shortener-1-l8gp.onrender.com/${surl}`}
                         </a>
                     </div>
                 )}
